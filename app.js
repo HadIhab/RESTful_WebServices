47ab7008cfdd72c82f8635791991c131e28ed724
HadIhab <chocovani101@hotmail.fr>
2021-04-02 21:45:57 +0100

    Creating mongo database & rows

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+
+bookRouter.route('/books')
+  .get((req,res)=>{
+  	const response = {hello:'This is my book'};
+  	res.json(response);
+  });
+app.use('/api',bookRouter);
 
 app.get('/',(req,res)=>{
 	res.send('Home page Nodemon API.');
