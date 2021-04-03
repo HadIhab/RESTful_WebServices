831a8633a5b0bf3b2bd05aaef7e4a378da497719
HadIhab <chocovani101@hotmail.fr>
2021-04-03 16:34:34 +0100

    Adding Middleware

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -22,27 +22,28 @@ function routes(Book) {
         return res.json(books);
       });
     });
-  bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(book);
-      });
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
     });
+  });  
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => { return res.json(req.book); })
     .put((req,res)=>{
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          return res.send(err);
-        }
+        const { book } = req;
         book.title = req.body.title;
         book.author = req.body.author;
         book.genre = req.body.genre;
         book.read = req.body.read;
         book.save();
         return res.json(book);
-      });
     })
 
   return bookRouter;
