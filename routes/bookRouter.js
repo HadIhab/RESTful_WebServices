eecffb7bf1738b835b07fe3578b7b4adef3bc02e
HadIhab <chocovani101@hotmail.fr>
2021-04-03 14:12:23 +0100

    Fixing the Issue

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,35 +1,36 @@
 const express = require('express');
 
-function routes(Book){
-  const bookRouter = express.Router;	
+function routes(Book) {
+  const bookRouter = express.Router();
   bookRouter.route('/books')
-  .post((req,res)=>{
-  	const book = new Book(req.body);
-  	book.save();
-  	return res.status(201).json(book);
-  })	
-  .get((req,res)=>{
-  	const query = {};
-  	if(req.query.genre){
-  		query.genre = req.query.genre
-  	}
-  	Book.find(query,(err,books) => {
-  		if (err){
-  			return res.send(err);
-  		}
-  		return res.json(books);
-  	});
-  });
-bookRouter.route('/books/:bookId')
-  .get((req,res)=>{
+    .post((req, res) => {
+      const book = new Book(req.body);
 
-  	Book.findById(req.params.bookId,(err,book) => {
-  		if (err){
-  			return res.send(err);
-  		}
-  		return res.json(book);
-  	});
-  }); 
+      book.save();
+      return res.status(201).json(book);
+    })
+    .get((req, res) => {
+      const query = {};
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      }
+      Book.find(query, (err, books) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(books);
+      });
+    });
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+      Book.findById(req.params.bookId, (err, book) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    });
+
   return bookRouter;
 }
 
