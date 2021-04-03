92dbfcb610383a7f40f9bbf635c211cd11a53fb8
HadIhab <chocovani101@hotmail.fr>
2021-04-03 11:28:53 +0100

    Setting up request body

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true,useUnifiedTopology: true });
 const port = process.env.PORT || 3000;
 const bookRouter = express.Router();
-const Book = require('./models/bookModel')
+const Book = require('./models/bookModel');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 bookRouter.route('/books')
+  .post('', ()=>{
+
+  })	
   .get((req,res)=>{
   	const query = {};
   	if(req.query.genre){
@@ -22,7 +28,7 @@ bookRouter.route('/books')
   });
 bookRouter.route('/books/:bookId')
   .get((req,res)=>{
-  	
+
   	Book.findById(req.params.bookId,(err,book) => {
   		if (err){
   			return res.send(err);
